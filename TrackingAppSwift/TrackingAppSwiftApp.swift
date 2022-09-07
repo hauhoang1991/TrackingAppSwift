c173b6d7ce038a9f6eecdba3061599870176a8cf
Hau Hoang <hauhoangduc@gmail.com>
2022-09-07 10:44:40 -0700

    Initial Commit

---
+++ TrackingAppSwift/TrackingAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackingAppSwiftApp.swift
+//  TrackingAppSwift
+//
+//  Created by Hau Hoang on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackingAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
